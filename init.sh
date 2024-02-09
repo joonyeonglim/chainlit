b6f042aedcf1d43e57271daa68a0878472fba546
joonyeong lim <joonyeonglim@joonyeongui-MacBookAir.local>
2024-02-09 17:15:08 +0900

    ADD init.sh

---
+++ init.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PACKAGE=$(python3 -c "import site; print(site.getsitepackages()[0])")
+CHAINLIT_HOME=$PACKAGE/chainlit
+
+cd ./backend && pip install .
+
+sudo mkdir -p "$CHAINLIT_HOME"/frontend/dist
+sudo ln -sf "$PWD"/frontend/dist/* "$CHAINLIT_HOME"/frontend/dist
+
+sudo mkdir -p "$CHAINLIT_HOME"/copilot/dist
+sudo ln -sf "$PWD"/libs/copilot/dist/* "$CHAINLIT_HOME"/copilot/dist
